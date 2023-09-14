6f2ffbe9d2a754471f1bd20a6c3976e8df0f49cb
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2023-09-13 22:57:11 -0400

    created a new style called logo for ParkSmart APp

--- App.js
+++ App.js
@@ -4,13 +4,18 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.logo}>PS</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  logo: {
+    fontSize: "64px",
+    fontWeight: 'bold',
+    color: "blue",
+  },
   container: {
     flex: 1,
     backgroundColor: '#fff',
