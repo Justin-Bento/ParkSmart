98405d0daa3c7e11d50aede1e2e0073ba208104c
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-30 11:52:14 -0400

    Created a new page and tab called dashboard

---
+++ app/(tabs)/dashboard.tsx
@@ -0,0 +1,19 @@
+import { ScrollView, Text, View } from "react-native";
+
+export default function Dashboard() {
+  return (
+    <>
+      <ScrollView className="w-11/12 mx-auto pt-8 prose">
+        <View className="space-y-2">
+          <Text className="text-2xl font-bold">Welcome, Dashboard Page.</Text>
+          <Text className="leading-6 tracking-wide">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis assumenda inventore nostrum, similique
+            aliquid necessitatibus! Officiis sunt quas recusandae quae, molestias ullam aut est fugiat. Quod, vel.
+            Inventore, neque pariatur.
+          </Text>
+          <Text className="w-full aspect-video bg-gray-400 rounded">&nbsp;</Text>
+        </View>
+      </ScrollView>
+    </>
+  );
+}
