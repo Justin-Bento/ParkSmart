25cde133e37919bc51f4e73232304e9ac9f63e20
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-30 10:14:42 -0400

    Imported global css stylesheet and formatted document with prettier.

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import "./global.css";
 
 export default function App() {
   return (
@@ -13,13 +14,13 @@ export default function App() {
 const styles = StyleSheet.create({
   logo: {
     fontSize: "64px",
-    fontWeight: 'bold',
+    fontWeight: "bold",
     color: "blue",
   },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
