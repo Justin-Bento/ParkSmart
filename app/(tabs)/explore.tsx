b3469d35e3c8e73b6f062874e4bf8aac586a6639
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-30 11:49:53 -0400

    Added container and typography styles.

--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -3,9 +3,15 @@ import { ScrollView, Text, View } from "react-native";
 export default function Explore() {
   return (
     <>
-      <ScrollView className="prose">
-        <View>
-          <Text>Welcome, Explore Page.</Text>
+      <ScrollView className="w-11/12 mx-auto pt-8 prose">
+        <View className="space-y-2">
+          <Text className="text-2xl font-bold">Welcome, Explore Page.</Text>
+          <Text className="leading-6 tracking-wide">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis assumenda inventore nostrum, similique
+            aliquid necessitatibus! Officiis sunt quas recusandae quae, molestias ullam aut est fugiat. Quod, vel.
+            Inventore, neque pariatur.
+          </Text>
+          <Text className="w-full aspect-video bg-gray-400 rounded">&nbsp;</Text>
         </View>
       </ScrollView>
     </>
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,17 @@
 import { ScrollView, Text, View } from "react-native";
 
-export default function Explore() {
+export default function Home() {
   return (
     <>
-      <ScrollView className="prose">
-        <View>
-          <Text>Welcome, Home Page.</Text>
+      <ScrollView className="w-11/12 mx-auto pt-8 prose">
+        <View className="space-y-2">
+          <Text className="text-2xl font-bold">Welcome, Home Page.</Text>
+          <Text className="leading-6 tracking-wide">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis assumenda inventore nostrum, similique
+            aliquid necessitatibus! Officiis sunt quas recusandae quae, molestias ullam aut est fugiat. Quod, vel.
+            Inventore, neque pariatur.
+          </Text>
+          <Text className="w-full aspect-video bg-gray-400 rounded">&nbsp;</Text>
         </View>
       </ScrollView>
     </>
