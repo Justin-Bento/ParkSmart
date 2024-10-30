9561435ce6ed8e4543c858113d8259813f638db3
Justin Bento <44421842+Justin-Bento@users.noreply.github.com>
2024-10-30 10:01:23 -0400

    updated app and used expo to add new assets

---
+++ App.js
@@ -0,0 +1,25 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.logo}>PS</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  logo: {
+    fontSize: "64px",
+    fontWeight: 'bold',
+    color: "blue",
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
